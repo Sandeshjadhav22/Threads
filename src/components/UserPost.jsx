8e56c6efe559ac398aa048a5e0f7f8aae9a9f7f9
sandy <sandeshjadhav2002@gmail.com>
2024-03-05 21:23:31 +0530

    User page done

--- src/components/UserPost.jsx
+++ src/components/UserPost.jsx
@@ -1,9 +1,87 @@
+import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { BsThreeDots } from "react-icons/bs";
+import Actions from "./Actions";
+import { useState } from "react";
 
-
-const UserPost = () => {
+const UserPost = ({likes, replies, postImg, postTitle}) => {
+  const [liked, setLiked] = useState(false)
   return (
-    <div>UserPost here we can add posts </div>
-  )
-}
+    <Link to={"/markzuckerberg/post/1"}>
+      <Flex gap={3} mb={4} py={5}>
+        <Flex flexDirection={"column"} alignItems={"center"}>
+          <Avatar size={"md"} name="Mark zuckerburg" src="/zuck-avatar.png" />
+          <Box w="1px" h={"full"} bg={"gray.light"} my={2}></Box>
+          <Box position={"relative"} w={"full"}>
+            <Avatar
+              size="xs"
+              name="jhoe doe"
+              src="https://bit.ly/dan-abramov"
+              position={"absolute"}
+              top={"0px"}
+              left="15px"
+              p={"2px"}
+            />
+            <Avatar
+              size="xs"
+              name="jhoe doe"
+              src="https://bit.ly/prosper-baba"
+              position={"absolute"}
+              bottom={"0px"}
+              right="-5px"
+              p={"2px"}
+            />
+            <Avatar
+              size="xs"
+              name="jhoe doe"
+              src="https://bit.ly/sage-adebayo"
+              position={"absolute"}
+              bottom={"0px"}
+              left="4px"
+              p={"2px"}
+            />
+          </Box>
+        </Flex>
+        <Flex flex={1} flexDirection={"column"} gap={2}>
+          <Flex justifyContent={"space-between"} w={"full"}>
+            <Flex w={"full"} alignItems={"center"}>
+              <Text fontSize={"sm"} fontWeight={"bold"}>
+              Mark Zuckerberg
+              </Text>
+              <Image src="/verified.png" w={4} h={4} ml={1} />
+            </Flex>
+            <Flex gap={4} alignItems={"center"}>
+              <Text fontSize={"sm"} color={"gray.light"}>
+                1d
+              </Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
+          <Text fontSize={"sm"}>{postTitle}</Text>
+          {postImg && (
 
-export default UserPost
+            <Box
+            borderRadius={6}
+            overflow={"hidden"}
+            border={"1px solid "}
+            borderColor={"gray.light"}
+            >
+            <Image src={postImg} w={"full"} />
+          </Box>
+            )}
+
+          <Flex gap={3} my={1}>
+            <Actions liked={liked} setLiked={setLiked}/>
+          </Flex>
+          <Flex gap={2} alignItems={"center"}>
+            <Text color={"gray.light"} fontSize="sm">{replies} replies</Text>
+            <Box w={0.5} h={0.5} borderRadius={"full"} bg={'gray.light'}></Box>
+            <Text color={"gray.light"} fontSize="sm">{likes} likes</Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Link>
+  );
+};
+
+export default UserPost;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,7 +5,11 @@ const UserPage = () => {
   return (
     <>
     <UserHeader/>
-    <UserPost/>
+    <UserPost  likes={1200} replies={4} postImg="/post1.png" postTitle="Lets talk about threads"/>
+    <UserPost  likes={450} replies={41} postImg="/post2.png" postTitle="Nice tutorial"/>
+    <UserPost  likes={152} replies={81} postImg="/post3.png" postTitle="I love this guy"/>
+    <UserPost  likes={483} replies={34}  postTitle="This is my first Post"/>
+    
     </>
   )
 }
