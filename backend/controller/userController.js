247a4ce31cf8cf11347f7a6cd3c1d2f6676039c6
sandy <sandeshjadhav2002@gmail.com>
2024-03-10 13:57:54 +0530

    Cloudinary setup Done

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,8 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import genrateTokenAndSetCookie from "../utils/helpers/genrateTokenAndSetCookie.js";
-
+import  {v2 as cloudinary} from "cloudinary"
+ 
 
 const getUserProfile = async(req,res) => {
    const {username} = req.params;
@@ -127,7 +128,9 @@ const followUnFollowUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  const { name, email, password, username, profilePic, bio } = req.body;
+  const { name, email, password, username, bio } = req.body;
+  let {profilePic} = req.body
+
   const userId = req.user._id;
   try {
     let user = await User.findById(userId);
@@ -140,6 +143,14 @@ const updateUser = async (req, res) => {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
       user.password = hashedPassword;
+    }
+
+    if(profilePic){
+      if(user.profilePic){
+        await cloudinary.uploader.destroy(user.profilePic.split("/").pop().split(".")[0])
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(profilePic)
+      profilePic = uploadedResponse.secure_url
     }
 
     user.name = name || user.name;
