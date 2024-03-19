4e4ae1d1a5a03c22f174faae432d60230198f04c
sandy <sandeshjadhav2002@gmail.com>
2024-03-19 14:44:00 +0530

    Like func done

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,7 +1,48 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom"
+import useShowToast from "../hooks/useShowToast";
 
-const Actions = ({ liked, setLiked }) => {
+const Actions = ({post:post_ }) => {
+	const user = useRecoilValue(userAtom);
+	const [liked, setLiked] = useState(post_.likes.includes(user?._id));
+    const showToast = useShowToast();
+
+	const [post, setPost] = useState(post_);
+	const [isLiking, setIsLiking] = useState(false)
+
+	const handleLikeAndunlike = async() =>{
+		if(!user) return showToast("Error", "You must loggin to like a post" , "error")
+		if(isLiking) return;
+		setIsLiking(true)
+		try {
+			const res = await fetch("/api/posts/like/" + post._id, {
+				method:"PUT",
+				headers:{
+					"Content-Type":"application/json"
+				},
+			})
+			const data = await res.json();
+			if(data.error) return showToast("Error",error.message,"error")
+			
+
+			if(!liked){
+				setPost({...post,likes: [...post.likes,user._id]})
+			}else{
+				setPost({...post, likes: post.likes.filter(id=> id !== user._id)})
+			}
+
+			setLiked(!liked)
+		} catch (error) {
+			showToast("Error",error.message,"error")
+		} finally{
+			setIsLiking(false)
+		}
+	}
+
 	return (
+		<Flex flexDirection="column">
 		<Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
 			<svg
 				aria-label='Like'
@@ -11,7 +52,7 @@ const Actions = ({ liked, setLiked }) => {
 				role='img'
 				viewBox='0 0 24 22'
 				width='20'
-				onClick={() => setLiked(!liked)}
+				onClick={handleLikeAndunlike}
 			>
 				<path
 					d='M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z'
@@ -75,6 +116,17 @@ const Actions = ({ liked, setLiked }) => {
 					strokeWidth='2'
 				></polygon>
 			</svg>
+			</Flex>
+
+			<Flex gap={2} alignItems={"center"}>
+				<Text color={"gray.light"} fontSize='sm'>
+					{post.replies.length} replies
+				</Text>
+				<Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+				<Text color={"gray.light"} fontSize='sm'>
+					{post.likes.length} likes
+				</Text>
+			</Flex>
 		</Flex>
 	);
 };
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -7,7 +7,6 @@ import useShowToast from "../hooks/useShowToast";
 import {formatDistanceToNow} from 'date-fns' 
 
 const Post = ({ post, postedBy }) => {
-  const [liked, setLiked] = useState(false);
   const [user, setUser] = useState(null);
   const showToast = useShowToast();
 
@@ -18,7 +17,6 @@ const Post = ({ post, postedBy }) => {
       try {
         const res = await fetch("/api/users/profile/" + postedBy);
         const data = await res.json();
-        console.log(data);
         if (data.error) {
           showToast("Error", data.error, "error");
           return;
@@ -119,17 +117,11 @@ const Post = ({ post, postedBy }) => {
           )}
 
           <Flex gap={3} my={1}>
-            <Actions liked={liked} setLiked={setLiked} />
+            <Actions post={post} />
           </Flex>
-          <Flex gap={2} alignItems={"center"}>
-            <Text color={"gray.light"} fontSize="sm">
-              {post.replies.lenght} replies
-            </Text>
-            <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-            <Text color={"gray.light"} fontSize="sm">
-              {post.likes.lenght} likes
-            </Text>
-          </Flex>
+
+
+          
         </Flex>
       </Flex>
     </Link>
